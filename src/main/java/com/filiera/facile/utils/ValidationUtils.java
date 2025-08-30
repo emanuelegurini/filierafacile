c0a276b3deb93883a616e7d33e039afc1cbc45db
Aleaskn <alessandro.cristalli@hotmail.it>
2025-08-30 18:28:30 +0200

    feat: add DefaultUtente with validation and pom dependency

---
+++ src/main/java/com/filiera/facile/utils/ValidationUtils.java
@@ -0,0 +1,32 @@
+package com.filiera.facile.utils;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public final class ValidationUtils {
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(
+            "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$"
+    );
+
+    private  ValidationUtils(){}
+
+    public static String validateEmail(String email) {
+        Objects.requireNonNull(email, "Email non può essere null");
+        String trimmedEmail = email.trim();
+
+        if (trimmedEmail.isEmpty()) {
+            throw new IllegalArgumentException("Email non può essere vuota");
+        }
+        if (!trimmedEmail.contains("@")) {
+            throw new IllegalArgumentException("Il formato dell'email non è valido");
+        }
+
+        if (!EMAIL_PATTERN.matcher(trimmedEmail).matches()) {
+            throw new IllegalArgumentException("Il formato dell'email non è valido");
+        }
+
+        return trimmedEmail;
+    }
+
+}
