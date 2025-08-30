3a32c81f8752fc327ff645bd57480263f121a947
Aleaskn <alessandro.cristalli@hotmail.it>
2025-08-30 17:09:53 +0200

    feat : add enumerations

--- src/main/java/com/filiera/facile/model/enums/MetodoColtivazione.java
+++ src/main/java/com/filiera/facile/model/enums/MetodoColtivazione.java
@@ -1,4 +1,37 @@
 package com.filiera.facile.model.enums;
 
+/**
+ * Definisce i principali metodi di coltivazione per le materie prime.
+ */
 public enum MetodoColtivazione {
+    /**
+     * Senza pesticidi chimici, solo sostanze naturali.
+     */
+
+    BIOLOGICO,
+
+    /**
+     * Approccio olistico che segue i cicli lunari e preparati naturali.
+     */
+    BIODINAMICO,
+
+    /**
+     * Uso ridotto e mirato di chimici, combinato con pratiche ecologiche.
+     */
+    AGRICOLTURA_INTEGRATA,
+
+    /**
+     * Coltivazione intensiva con largo uso di chimici e fertilizzanti.
+     */
+    CONVENZIONALE,
+
+    /**
+     * Coltura fuori suolo con nutrienti disciolti in acqua..
+     */
+    IDROPONICO,
+
+    /**
+     * Sistema sostenibile che imita gli ecosistemi naturali.
+     */
+    PERMACULTURA
 }
